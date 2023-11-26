c4f6e85cff7eddb308f1e1123ae11daf6690f5fc
Kamil <kamilkoww@gmail.com>
2023-11-26 16:18:26 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Kamil on 26/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
